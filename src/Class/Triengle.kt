24c641acc66488500d3d390e9f23d822ec2ae6c4
ZakharFilip <Zaxar_zah@mail.ru>
2024-11-04 16:31:11 +0300

    NewOn0411

--- src/Class/Triengle.kt
+++ src/Class/Triengle.kt
@@ -1,6 +1,22 @@
 package Class
 
+import java.util.*
 class Triengle(val a: Dot, val b: Dot, val c: Dot)
 {
+
+    private fun area(p1: Dot, p2: Dot, p3: Dot): Double {
+        return Math.abs((p1.x *(p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) / 2.0)
+    }
+
+    fun contains(point: Dot): Boolean {
+
+        val areaABC = area(a, b, c)
+        val areaPAB = area(point, a, b)
+        val areaPBC = area(point, b, c)
+        val areaPCA = area(point, c, a)
+
+
+        return areaABC == areaPAB + areaPBC + areaPCA
+    }
 
 }
--- src/Class/TriengleAndDot.kt
+++ src/Class/TriengleAndDot.kt
@@ -2,5 +2,33 @@ package Class
 
 fun main() {
 
+    println("Введите координаты треугольника:")
+    println("Вершина A (x1):")
+    val Ax1=readLine()
+    println("Вершина A  (y1):")
+    val Ax2=readLine()
+    println("Вершина B (x2):")
+    val Bx1=readLine()
+    println("Вершина B (y2):")
+    val Bx2=readLine()
+    println("Вершина C (x3):")
+    val Cx1=readLine()
+    println("Вершина C (y3):")
+    val Cx2= readLine()
 
+    val triangle = Triengle(Dot(Ax1!!.toDouble(), Ax2!!.toDouble()), Dot(Bx1!!.toDouble(), Bx2!!.toDouble()), Dot(Cx1!!.toDouble(), Cx2!!.toDouble()))
+
+    println("Введите координаты точки (x):")
+    val Px= readln()
+    println("Введите координаты точки (y):")
+    val  Py = readln()
+
+    val point = Dot(Px!!.toDouble(), Py!!.toDouble())
+
+
+    if (triangle.contains(point)) {
+        println("Точка ${point} находится внутри треугольника.")
+    } else {
+        println("Точка ${point} находится вне треугольника.")
+    }
 }
